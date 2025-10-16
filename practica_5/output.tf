85ee8e421a2c363c726c2aae2f6e386011136b76
IVAN GEOVANNI ORTEGA ALVA <c059473@produban.com.mx>
2025-10-16 16:37:25 -0600

    add state ec2 private 1

--- practica_5/output.tf
+++ practica_5/output.tf
@@ -7,3 +7,9 @@ output "ec2_public_ip_2" {
    description = "Public IP of the EC2 instance"
    value = aws_instance.public_instance_2.public_ip
 }
+
+output "ec2_private_state_1" {
+   description = "Private IP of the EC2 instance"
+   value = aws_instance.private_instance_1.instance_state
+  
+}
