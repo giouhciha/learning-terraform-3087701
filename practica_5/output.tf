69d0560a4b4d0f828818ca663737d4e2b6ded11c
IVAN GEOVANNI ORTEGA ALVA <c059473@produban.com.mx>
2025-10-16 16:13:13 -0600

    fix output

--- practica_5/output.tf
+++ practica_5/output.tf
@@ -1,9 +1,4 @@
-output "ec2_public_ip_instance_1" {
+output "ec2_public_ip" {
    description = "Public IP of the EC2 instance"
-   value = aws_instance.public_instance.public_instance_1.public_ip
-}
-
-output "ec2_public_ip_instance_2" {
-   description = "Public IP of the EC2 instance"
-   value = aws_instance.public_instance.public_instance_2.public_ip
+   value = aws_instance.public_instance_1.public_ip
 }
