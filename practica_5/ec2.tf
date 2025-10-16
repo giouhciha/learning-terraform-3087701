e52bbf8733f640eeab4453ed42696fcdd5bbeef9
IVAN GEOVANNI ORTEGA ALVA <c059473@produban.com.mx>
2025-10-16 16:26:23 -0600

    Se creo la tarea para ssm

--- practica_5/ec2.tf
+++ practica_5/ec2.tf
@@ -24,6 +24,8 @@ resource "aws_instance" "private_instance_1" {
   instance_type = "t3.micro"
   subnet_id = aws_subnet.subnet_private.id
 
+  iam_instance_profile = aws_iam_instance_profile.ssm_instance_profile.name
+
   tags = {
     Name = "backend Instance 1"
   }
