38dcb3f35e3f9eb5c04ba8d0067e7fb77374ab39
IVAN GEOVANNI ORTEGA ALVA <c059473@produban.com.mx>
2025-10-16 16:07:53 -0600

    Pruebas con mas instacias de ec2

--- practica_5/ec2.tf
+++ practica_5/ec2.tf
@@ -1,9 +1,42 @@
-#resource "aws_instance" "public_instance" {
-#  ami           = "ami-0341d95f75f311023"
-#  instance_type = "t3.micro"
-#  subnet_id = aws_subnet.subnet_publica.id
-#
-#  tags = {
-#    Name = "Publica Instance"
-#  }
-#}
+resource "aws_instance" "public_instance_1" {
+  ami           = "ami-0341d95f75f311023"
+  instance_type = "t3.micro"
+  subnet_id = aws_subnet.subnet_publica.id
+
+  tags = {
+    Name = "Publica Instance 1"
+  }
+}
+
+resource "aws_instance" "public_instance_2" {
+  ami           = "ami-0341d95f75f311023"
+  instance_type = "t3.micro"
+  subnet_id = aws_subnet.subnet_publica.id
+
+  tags = {
+    Name = "public Instance 2"
+  }
+  
+}
+
+resource "aws_instance" "private_instance_1" {
+  ami           = "ami-0341d95f75f311023"
+  instance_type = "t3.micro"
+  subnet_id = aws_subnet.subnet_private.id
+
+  tags = {
+    Name = "backend Instance 1"
+  }
+  
+}
+
+resource "aws_instance" "private_instance_2" {
+  ami           = "ami-0341d95f75f311023"
+  instance_type = "t3.micro"
+  subnet_id = aws_subnet.subnet_private.id
+
+  tags = {
+    Name = "backend Instance 2"
+  }
+  
+}
