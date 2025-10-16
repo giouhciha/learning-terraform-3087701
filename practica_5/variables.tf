fb591a8d39d9c3aaab8012da0284a8aa8b875780
IVAN GEOVANNI ORTEGA ALVA <c059473@produban.com.mx>
2025-10-16 11:30:59 -0600

    Eliminacion toda infra

--- practica_5/variables.tf
+++ practica_5/variables.tf
@@ -1,8 +1,8 @@
-variable "virginia_cidr" {
-    description = "CIDR Viginia"
-    type       = string
-    sensitive = false
-}
+#variable "virginia_cidr" {
+#    description = "CIDR Viginia"
+#    type       = string
+#    sensitive = false
+#}
 
 #variable "ohio_cidr" {
 #
