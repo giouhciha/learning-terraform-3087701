0af54eaef9076e11583c917e7c286d1aa65ae34b
IVAN GEOVANNI ORTEGA ALVA <c059473@produban.com.mx>
2025-10-16 12:25:15 -0600

    Se agregan etiquetas a las subnets

--- practica_5/vpc.tf
+++ practica_5/vpc.tf
@@ -11,9 +11,17 @@ resource "aws_subnet" "subnet_publica" {
   vpc_id                  = aws_vpc.vpc_virginia.id
   cidr_block              = var.subnets[0]
   map_public_ip_on_launch = true
+  tags = {
+    Name = "Public Subnet"
+    env  = "Dev"
+  }
 }
 
 resource "aws_subnet" "subnet_private" {
   vpc_id            = aws_vpc.vpc_virginia.id
   cidr_block        = var.subnets[1]
+  tags = {
+    Name = "Private Subnet"
+    env  = "Dev"
+  }
 }
