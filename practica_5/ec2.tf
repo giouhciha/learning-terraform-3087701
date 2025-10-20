e9d7ef36f7590d8e7e035d35269a914c9612563e
IVAN GEOVANNI ORTEGA ALVA <c059473@produban.com.mx>
2025-10-20 10:37:02 -0600

    correccion de ec2 para la ami

--- practica_5/ec2.tf
+++ practica_5/ec2.tf
@@ -9,7 +9,7 @@
 #}
 
 resource "aws_instance" "private_instance" {
-  ami           = var.amis["us-east-2"]
+  ami           = var.amis["ohio"]
   instance_type = "t3.micro"
   subnet_id = aws_subnet.subnet_private.id
   key_name = data.aws_key_pair.deployer_key.key_name
