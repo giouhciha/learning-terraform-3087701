e9a76f4c1575c45b1b5baf239fb3540e19c980dd
IVAN GEOVANNI ORTEGA ALVA <c059473@produban.com.mx>
2025-10-20 08:36:51 -0600

    Terminamos laboratorio

--- practica_5/ec2.tf
+++ practica_5/ec2.tf
@@ -1,22 +1,21 @@
-resource "aws_instance" "public_instance" {
-  ami           = "ami-0341d95f75f311023"
-  instance_type = "t3.micro"
-  subnet_id = aws_subnet.subnet_publica.id
+#resource "aws_instance" "public_instance" {
+#  ami           = "ami-0341d95f75f311023"
+#  instance_type = "t3.micro"
+#  subnet_id = aws_subnet.subnet_publica.id
+#
+#  tags = {
+#    Name = "Publica Instance"
+#  }
+#}
 
-  tags = {
-    Name = "Publica Instance"
-  }
-}
-
-resource "aws_instance" "private_instance" {
-  ami           = "ami-0341d95f75f311023"
-  instance_type = "t3.micro"
-  subnet_id = aws_subnet.subnet_private.id
-
-  iam_instance_profile = aws_iam_instance_profile.ssm_instance_profile.name
-
-  tags = {
-    Name = "backend Instance"
-  }
-  
-}
+#resource "aws_instance" "private_instance" {
+#  ami           = "ami-0341d95f75f311023"
+#  instance_type = "t3.micro"
+#  subnet_id = aws_subnet.subnet_private.id
+#
+#  iam_instance_profile = aws_iam_instance_profile.ssm_instance_profile.name
+#
+#  tags = {
+#    Name = "backend Instance"
+#  }
+#}
