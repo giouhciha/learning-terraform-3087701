c785b619e3d64b48b6c89fa86ca6acda63fd897a
IVAN GEOVANNI ORTEGA ALVA <c059473@produban.com.mx>
2025-10-20 10:05:58 -0600

    Creamos solo la vpc con subnets

--- practica_5/ec2.tf
+++ practica_5/ec2.tf
@@ -8,15 +8,15 @@
 #  }
 #}
 
-resource "aws_instance" "private_instance" {
-  ami           = var.amis["us-east-2"]
-  instance_type = "t3.micro"
-  subnet_id = aws_subnet.subnet_private.id
-  key_name = data.aws_key_pair.deployer_key.key_name
+#resource "aws_instance" "private_instance" {
+#  ami           = var.amis["us-east-2"]
+#  instance_type = "t3.micro"
+#  subnet_id = aws_subnet.subnet_private.id
+#  key_name = data.aws_key_pair.deployer_key.key_name
 
   #iam_instance_profile = aws_iam_instance_profile.ssm_instance_profile.name
 
-  tags = {
-    Name = "backend Instance"
-  }
-}
+#  tags = {
+#    Name = "backend Instance"
+#  }
+#}
