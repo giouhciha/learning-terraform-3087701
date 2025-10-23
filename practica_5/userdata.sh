d7ea751428861dac7bb56b72e3017f684862b082
IVAN GEOVANNI ORTEGA ALVA <c059473@produban.com.mx>
2025-10-23 12:58:08 -0600

    se actualiza el script

--- practica_5/userdata.sh
+++ practica_5/userdata.sh
@@ -1,6 +1,7 @@
-echo "Este es un mensaje" > > ~/hello.txt
+#!/bin/bash
+echo "Este es un mensaje"  > ~/hello.txt
 yum update -y
 yum install -y httpd
+echo "Hola desde la instancia Apache" > /var/www/html/index.html
 systemctl enable httpd
 systemctl start httpd
-yum update -y
