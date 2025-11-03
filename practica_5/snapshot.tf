57d1d8906afbec13aaff8934b2ef30f81ccfdce8
IVAN GEOVANNI ORTEGA ALVA <c059473@produban.com.mx>
2025-11-03 10:59:29 -0600

    se define el recurso para el snapshot

--- practica_5/snapshot.tf
+++ practica_5/snapshot.tf
@@ -1,7 +1,17 @@
 #resource "aws_ebs_snapshot" "snap-public_instance" {
-#  volume_id = aws_ebs_volume.example.id
-#
+#  volume_id = vol-02a581381eb2fbeb2
+
 #  tags = {
 #    Name = "HelloWorld_snap"
 #  }
 #}
+
+resource "aws_ebs_snapshot" "snap_public_instance" {
+  for_each = { for idx, instance in aws_instance.public_instance : idx => instance }
+
+  volume_id = each.value.root_block_device[0].volume_id
+
+  tags = {
+    Name = "HelloWorld_snap_${each.key}"
+  }
+}
